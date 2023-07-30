408276e9c15c6d2323755722f8354aad3a719c7d
VShawn <veckshawn@gmail.com>
2023-07-30 13:52:19 +0800

    feat: write to PasswordVault

--- WpfWindowsHelloLoginApp/MainWindow.xaml.cs
+++ WpfWindowsHelloLoginApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -35,7 +36,25 @@ namespace WpfWindowsHelloLoginApp
                 switch (result.Status)
                 {
                     case KeyCredentialStatus.Success:
-                        MessageBox.Show("Logged in.");
+                        var resourceName = "My App";
+                        Windows.Security.Credentials.PasswordCredential? pass = null;
+                        var vault = new Windows.Security.Credentials.PasswordVault(); 
+                        try
+                        {
+                            //var credentialList = vault.FindAllByResource(resourceName);
+                            //if (credentialList?.Any() == true)
+                            //{
+                            //    var credential = credentialList[0];
+                            //    credential.RetrievePassword();
+                            //    MessageBox.Show(credential?.Password ?? "null");
+                            //}
+                            pass = vault.Retrieve(resourceName, "last time");
+                        }
+                        catch (Exception)
+                        {
+                        }
+                        vault.Add(new Windows.Security.Credentials.PasswordCredential(resourceName, "last time", DateTime.Now.ToString("R")));
+                        MessageBox.Show("Logged in." + " last pass = " + pass?.Password ?? "null");
                         break;
                     case KeyCredentialStatus.UserCanceled:
                         MessageBox.Show("Login cancelled.");
