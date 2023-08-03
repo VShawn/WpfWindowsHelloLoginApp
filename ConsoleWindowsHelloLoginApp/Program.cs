222958d67e48de0138c0a2dd75fafb69772bea66
Shawn <veckshawn@gmail.com>
2023-08-03 13:47:43 +0800

    add CredUIPromptForWindowsCredentials

---
+++ ConsoleWindowsHelloLoginApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Security.Credentials;
+
+namespace ConsoleWindowsHelloLoginApp
+{
+    internal class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            await ButtonLogin_OnClick();
+            return 1;
+        }
+
+
+        private static async Task<int> ButtonLogin_OnClick()
+        {
+            bool supported = await KeyCredentialManager.IsSupportedAsync();
+            if (supported)
+            {
+                var result = await KeyCredentialManager.RequestCreateAsync("login", KeyCredentialCreationOption.ReplaceExisting);
+                switch (result.Status)
+                {
+                    case KeyCredentialStatus.Success:
+                        var resourceName = "My App";
+                        Windows.Security.Credentials.PasswordCredential? pass = null;
+                        var vault = new Windows.Security.Credentials.PasswordVault();
+                        try
+                        {
+                            //var credentialList = vault.FindAllByResource(resourceName);
+                            //if (credentialList?.Any() == true)
+                            //{
+                            //    var credential = credentialList[0];
+                            //    credential.RetrievePassword();
+                            //    Console.WriteLine(credential?.Password ?? "null");
+                            //}
+                            pass = vault.Retrieve(resourceName, "last time");
+                        }
+                        catch (Exception)
+                        {
+                        }
+                        vault.Add(new Windows.Security.Credentials.PasswordCredential(resourceName, "last time", DateTime.Now.ToString("R")));
+                        Console.WriteLine("Logged in." + " last pass = " + pass?.Password ?? "null");
+                        break;
+                    case KeyCredentialStatus.UserCanceled:
+                        Console.WriteLine("Login cancelled.");
+                        break;
+                    case KeyCredentialStatus.UnknownError:
+                    case KeyCredentialStatus.NotFound:
+                    case KeyCredentialStatus.UserPrefersPassword:
+                    case KeyCredentialStatus.CredentialAlreadyExists:
+                    case KeyCredentialStatus.SecurityDeviceLocked:
+                    default:
+                        Console.WriteLine("Login failed.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Microsoft Passport is not setup!\nPlease go to Windows Settings and set up a PIN to use it.");
+            }
+
+            return 1;
+        }
+    }
+}
---
+++ WpfWindowsCredential/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Misuzilla.Security;
+using static Misuzilla.Security.CredentialUI;
+
+namespace WpfWindowsCredential
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClickPromptForCredentials(object sender, RoutedEventArgs e)
+        {
+            var ret = CredentialUI.PromptForCredentials("target name", "test", "tes message");
+        }
+
+        private void Button_OnClickPromptForWindowsCredentials(object sender, RoutedEventArgs e)
+        {
+            var ret = CredentialUI.PromptForWindowsCredentials("caption", "tes message");
+        }
+
+        private void Button_OnClickPrompt(object sender, RoutedEventArgs e)
+        {
+            var ret = CredentialUI.Prompt("caption", "tes message");
+        }
+        private void Button_OnClickPromptForCredentialsWithSecureString(object sender, RoutedEventArgs e)
+        {
+            var ret = CredentialUI.PromptForCredentialsWithSecureString("target name", "caption", "tes message");
+        }
+        private void Button_OnClickPromptForWindowsCredentialsWithSecureString(object sender, RoutedEventArgs e)
+        {
+            var ret = CredentialUI.PromptForWindowsCredentialsWithSecureString("caption", "tes message");
+        }
+    }
+}
